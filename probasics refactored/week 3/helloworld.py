e615dccc4475460656546480d6c0302d89f783c3
cdne <cduneata@gmail.com>
2019-11-28 14:50:18 +0200

    Add main function
    Created new function and formatted code

--- probasics refactored/week 3/helloworld.py
+++ probasics refactored/week 3/helloworld.py
@@ -2,13 +2,21 @@ import sys
 
 ARGUMENT = 1
 
+
 def hello_world():
     if len(sys.argv) > 1:
         print_funct(sys.argv[ARGUMENT])
     else:
         print_funct('World')
 
+
 def print_funct(name):
     print(f'Hello {name}!')
 
-hello_world()
+
+def main():
+    if __name__ == "__main__":
+        hello_world()
+
+
+main()
